c572b2de5b5bb4ca564bbde461da59d6db444f3d
nhoEm <nguyenquoctruong110605@gmail.com>
2025-08-29 13:07:13 +0700

    #38 API getALL user

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            arrUsers: [],
+        }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.data.errCode === 0) {
+            this.setState({
+                arrUsers: response.data.users,
+            })
+            console.log('data all user: ', this.state.arrUsers);
+        }
 
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center">Manager user</div>
+                <div className="users-table mt-3 mx-1">
+                    <table>
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th className='address'>Address</th>
+                            <th className='action'>Action</th>
+                        </tr>
+                        {arrUsers && arrUsers.map((item, index) => {
+                            return (
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td className='btn'>
+                                        <button className='btn-edit'><i className="fas fa-wrench"></i></button>
+                                        <button className='btn-delete'><i className="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+
+
+
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,7 @@ const handleLoginApi = (email, password) => {
     return axios.post('api/login', { email, password });
 }
 
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-user?id=${inputId}`);
+}
+export { handleLoginApi, getAllUsers };
