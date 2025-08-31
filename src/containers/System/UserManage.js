2697b7a8417f273b97c669881cad4e0e24015012
nhoEm <nguyenquoctruong110605@gmail.com>
2025-08-31 21:26:54 +0700

    #40 CRUD create

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,12 +3,15 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
 import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
             arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
@@ -23,12 +26,30 @@ class UserManage extends Component {
 
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleFromUserManage = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
 
     render() {
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromUserManage={this.toggleFromUserManage}
+                />
                 <div className="title text-center">Manager user</div>
+                <div className='mx-1'>
+                    <button className="btn btn-primary px-3" onClick={() => { this.handleAddNewUser() }}><i className="fas fa-plus"> </i>Add new user </button>
+                </div>
                 <div className="users-table mt-3 mx-1">
                     <table>
                         <tr>
